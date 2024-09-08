1f6ace0eb698f121faa82dffcf9e6373fefd325b
vicente-santander <127953756+vicente-santander@users.noreply.github.com>
2024-09-08 20:44:13 -0300

    primer cambio

---
+++ lib/pages/about_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Page'),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.start, // Mover el contenido hacia arriba
+              children: [
+                const SizedBox(height: 60), // Espacio superior ajustado
+                SvgPicture.asset(
+                  'assets/icons/8666609_user_icon.svg',
+                  semanticsLabel: 'user Logo',
+                  width: 100,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Hola, soy Vicente, estudiante de Ingeniería en desarrollo de videojuegos y realidad virtual, para esta ocasión tuve que hacer este codigo para dispositivos moviles.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Navigate back to previous page
+              },
+              child: const Text('Go Back to Home'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logger/logger.dart';
+import 'detail_page.dart'; // Importar DetailPage
 
 final logger = Logger();
 
@@ -38,12 +39,62 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // Función para determinar el mensaje y el ícono a mostrar
+  Widget _getGameStatus() {
+    String message;
+    Widget icon;
+
+    // Asignar mensaje e ícono dependiendo del valor del contador
+    if (_counter == 10) {
+      message = "¡Victoria!";
+      icon = SvgPicture.asset(
+        'assets/icons/8664871_thumbs_up_icon.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'Victoria Icon',
+      );
+    } else if (_counter == 5) {
+      message = "Derrota";
+      icon = SvgPicture.asset(
+        'assets/icons/8666595_x_icon.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'Derrota Icon',
+      );
+    } else {
+      message = "¡Sigue jugando!";
+      icon = SvgPicture.asset(
+        'assets/icons/8674409_ic_fluent_bot_regular_icon.svg',
+        width: 100,
+        height: 100,
+        semanticsLabel: 'bot_icon',
+      );
+    }
+
+    // Devolver tanto el mensaje como el ícono en una columna
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon, // Mostrar el ícono
+        const SizedBox(height: 16),
+        Text(
+          message,
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     if (!_hasLogged) {
       logger.i("Building MyHomePage");
       _hasLogged = true;
     }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
@@ -51,46 +102,66 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            SvgPicture.asset(
-              'assets/icons/8674409_ic_fluent_bot_regular_icon.svg',
-              semanticsLabel: 'robot Logo',
-              width: 100,
+            // Card con el estado del juego y el contador
+            Card(
+              color: const Color.fromARGB(0, 0, 0, 0), // Color de la Card
+              elevation: 1, // Elevación de la Card
+              margin: const EdgeInsets.all(16),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    // Mensaje del juego (Victoria, Derrota o Sigue jugando)
+                    _getGameStatus(),
+                    const SizedBox(height: 16),
+                    Text(
+                      '$_counter',
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Botones para incrementar, decrementar y resetear
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: _decrementCounter,
+                          icon: const Icon(Icons.arrow_downward),
+                        ),
+                        IconButton(
+                          onPressed: _resetCounter,
+                          icon: const Icon(Icons.refresh),
+                        ),
+                        IconButton(
+                          onPressed: _incrementCounter,
+                          icon: const Icon(Icons.arrow_upward),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-            const Text(
-              'Has presionado el botón esta cantidad de veces:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 16),
+            // Botón fuera de la Card
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DetailPage(),
+                  ),
+                ); // Navegar a DetailPage
+              },
+              child: const Text('Go to Detail Page'),
             ),
           ],
         ),
       ),
-      persistentFooterButtons: _buildPersistentFooterButtons(),
     );
-  }
-
-  List<Widget> _buildPersistentFooterButtons() {
-    return [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            onPressed: _decrementCounter,
-            icon: const Icon(Icons.arrow_downward),
-          ),
-          IconButton(
-            onPressed: _resetCounter,
-            icon: const Icon(Icons.refresh),
-          ),
-          IconButton(
-            onPressed: _incrementCounter,
-            icon: const Icon(Icons.arrow_upward),
-          ),
-        ],
-      ),
-    ];
   }
 }
